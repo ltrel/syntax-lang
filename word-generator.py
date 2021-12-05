072cc72c3e69712b392a81a35aa49eaededdb830
ltrel <79512656+ltrel@users.noreply.github.com>
2021-12-05 13:09:52 +1100

    Vary word lengths, begin more words with vowels

--- word-generator.py
+++ word-generator.py
@@ -5,15 +5,16 @@ from pprint import pprint
 vowels = 'aeiou'
 illegal_consonants = 'wqy'
 consonants = string.ascii_lowercase.translate(
-    str.maketrans('','', vowels + illegal_consonants)
-)
+    str.maketrans('', '', vowels + illegal_consonants))
 letters = vowels + consonants
 illegal_lasts = 'rh'
-last_replacements = consonants.translate(str.maketrans('','', illegal_consonants))
+last_replacements = consonants.translate(
+    str.maketrans('', '', illegal_consonants))
+
 
 def add_char(partial):
     if partial == '':
-        return random.choice(letters)
+        return random.choice(random.choice([consonants, vowels]))
     if partial[-1] in vowels:
         return partial + random.choice(consonants)
     elif partial[-1] in consonants:
@@ -21,14 +22,21 @@ def add_char(partial):
     else:
         raise Exception('Can\'t add character')
 
-def generate_word(partial=''):
-    new = add_char(partial)
-    if len(new) > 5:
-        if new[-1] in illegal_lasts:
-            new = new[:-1] + random.choice(last_replacements)
-        return new
-    else:
-        return generate_word1(new)
+
+def generate_word():
+    def generator(chars_remaining, partial):
+        if chars_remaining < 1:
+            return partial
+        else:
+            new = add_char(partial)
+            return generator(chars_remaining - 1, new)
+
+    length = random.randrange(2, 9)
+    word = generator(length, '')
+    if word[-1] in illegal_lasts:
+        word = word[:-1] + random.choice(last_replacements)
+    return word
+
 
 def word_class(word):
     classes = [
@@ -39,6 +47,7 @@ def word_class(word):
         'converter',
     ]
     return classes[hash(word) % len(classes)]
+
 
 words = [generate_word() for _ in range(10)]
 table = [(i, word_class(i)) for i in words]
